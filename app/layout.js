3fae175a6416f31c7008398861ce33425ff3f438
Prajwal Rangegowda <147972135+PrajwalRangegowda@users.noreply.github.com>
2025-02-02 11:18:02 +0530

    removed apple touch icon

--- app/layout.js
+++ app/layout.js
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Prajwal Rangegowda" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#000000" />
         <link rel="icon" href="/icons/icon-48x48.png" />
